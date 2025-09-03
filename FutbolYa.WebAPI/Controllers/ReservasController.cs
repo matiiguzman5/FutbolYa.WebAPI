8fd866ba4aedd506077377cd73987a22e9b9045e
nixss723 <nicolas.alonso@davinci.edu.ar>
2025-09-02 22:13:45 -0300

    Update rservascontrller
    De vuelta a 22 partidoscontroller.cs

--- FutbolYa.WebAPI/Controllers/ReservasController.cs
+++ FutbolYa.WebAPI/Controllers/ReservasController.cs
@@ -363,7 +363,7 @@ namespace FutbolYa.WebAPI.Controllers
             {
                 "F5" => 10,
                 "F7" => 14,
-                "F11" => 24,
+                "F11" => 22,
                 _ => 10
             };
 
